25ae1772dc80c42dcf87854d0013f949cdcbfabe
winniewandia <winniewandia63@gmail.com>
2023-10-23 14:53:50 +0300

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Python script that returns information about
+an employee's TODO list progress
+"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    users_url = 'https://jsonplaceholder.typicode.com/users'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    response = requests.get(todos_url).json()
+    users_response = requests.get(users_url).json()
+    name_id = sys.argv[1]
+    name = None
+    for j in users_response:
+        if j['id'] == int(sys.argv[1]):
+            name = j['username']
+    task_list = []
+    for i in response:
+        if i['userId'] == int(sys.argv[1]):
+            employee_dict = {
+                "task": i['title'], "completed": i['completed'], "username": name}
+            task_list.append(employee_dict)
+
+    dict_task = {str(name_id): task_list}
+    with open('{}.json'.format(name_id), mode='w') as f:
+        json.dump(dict_task, f)
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,2 +1,3 @@
 0. Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
 1. export data to CSV
+2. Export to JSON
