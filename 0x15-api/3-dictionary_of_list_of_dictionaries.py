d3a8414360612e474fb7648dc4aadeea70a58e13
winniewandia <winniewandia63@gmail.com>
2023-10-23 17:29:36 +0300

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,22 +7,23 @@ import requests
 import sys
 
 if __name__ == '__main__':
+    from collections import defaultdict
     users_url = 'https://jsonplaceholder.typicode.com/users'
     todos_url = 'https://jsonplaceholder.typicode.com/todos'
 
-    todos_response = requests.get(todos_url).json()
     users_response = requests.get(users_url).json()
 
-    task_list = []
+    task_list = defaultdict(list)
     dict_task = {}
     for user in users_response:
         user_id = user['id']
         user_name = user['username']
+        url = '{}?userId={}'.format(todos_url, user_id)
+        todos_response = requests.get(url).json()
         for i in todos_response:
             my_dict = {"username": user_name,
                        "task": i['title'], "completed": i['completed']}
-            task_list.append(my_dict)
-        dict_task[str(user_id)] = task_list
+            task_list[i['userId']].append(my_dict)
 
     with open('todo_all_employees.json', mode='w') as f:
-        json.dump(dict_task, f)
+        json.dump(task_list, f)
